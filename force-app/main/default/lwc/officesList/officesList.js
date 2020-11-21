780fb18066dc20b058c13d586507ccbbfc7f30b3
DYeshchenko <yeshchenko.danil@gmail.com>
2020-11-21 21:24:08 +0200

    Adding Labels to the officesList component

--- force-app/main/default/lwc/officesList/officesList.js
+++ force-app/main/default/lwc/officesList/officesList.js
@@ -6,7 +6,25 @@ import OFFICE_OBJECT from "@salesforce/schema/Office__c";
 import OFFICE_ROOM_OBJECT from "@salesforce/schema/Office_Room__c";
 import OFFICE_NAME_FIELD from "@salesforce/schema/Office__c.Name";
 
+import LIST_LABEL from "@salesforce/label/c.Offices_List";
+import NAME_LABEL from "@salesforce/label/c.Name";
+import ADDRESS_LABEL from "@salesforce/label/c.Address";
+import TOTAL_AREA_LABEL from "@salesforce/label/c.Total_Area";
+import PREVIOUS_LABEL from "@salesforce/label/c.Previous";
+import NEXT_LABEL from "@salesforce/label/c.Next";
+import ERROR_LABEL from "@salesforce/label/c.Error";
+
 export default class OfficesList extends NavigationMixin(LightningElement) {
+  label = {
+    LIST_LABEL,
+    NAME_LABEL,
+    ADDRESS_LABEL,
+    TOTAL_AREA_LABEL,
+    PREVIOUS_LABEL,
+    NEXT_LABEL,
+    ERROR_LABEL
+  };
+
   @api pageSize;
   pageToken = null;
   nextPageToken = null;
@@ -43,7 +61,10 @@ export default class OfficesList extends NavigationMixin(LightningElement) {
     }
   }
 
-  @wire(getListUi, { objectApiName: OFFICE_ROOM_OBJECT, listViewApiName: "All_Rooms_with_Area_and_Office" })
+  @wire(getListUi, {
+    objectApiName: OFFICE_ROOM_OBJECT,
+    listViewApiName: "All_Rooms_with_Area_and_Office"
+  })
   getOfficeRooms({ error, data }) {
     if (data) {
       this.officeRooms = data.records.records;
@@ -64,7 +85,8 @@ export default class OfficesList extends NavigationMixin(LightningElement) {
 
       for (let room in this.officeRooms) {
         if (
-          this.officeRooms[room].fields.Office__r.value.id === this.offices[office].id &&
+          this.officeRooms[room].fields.Office__r.value.id ===
+            this.offices[office].id &&
           this.officeRooms[room].fields.Area__c.value
         ) {
           totalArea += this.officeRooms[room].fields.Area__c.value;
