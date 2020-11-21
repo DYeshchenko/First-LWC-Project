01a8deb7b6a82b5dd82058a3d9f2ea9a18a2e476
DYeshchenko <yeshchenko.danil@gmail.com>
2020-11-21 22:38:50 +0200

    Adding Labels to the roomsList component + Bug fix

--- force-app/main/default/lwc/roomsList/roomsList.js
+++ force-app/main/default/lwc/roomsList/roomsList.js
@@ -9,19 +9,47 @@ import OFFICE_ROOM_OBJECT from "@salesforce/schema/Office_Room__c";
 import ID_FIELD from "@salesforce/schema/Office_Room__c.Id";
 import IS_RESERVED_FIELD from "@salesforce/schema/Office_Room__c.Is_reserved__c";
 
+import ROOMS_LIST_LABEL from "@salesforce/label/c.Rooms_List";
+import NUMBER_LABEL from "@salesforce/label/c.Number";
+import AREA_LABEL from "@salesforce/label/c.Area";
+import CAPACITY_LABEL from "@salesforce/label/c.Capacity";
+import RESERVED_LABEL from "@salesforce/label/c.Is_reserved";
+import CHANGE_BOOKING_LABEL from "@salesforce/label/c.Change_booking";
+import BOOKED_LABEL from "@salesforce/label/c.Booked";
+import FREE_LABEL from "@salesforce/label/c.Free";
+import BOOK_LABEL from "@salesforce/label/c.Book";
+import VACATE_LABEL from "@salesforce/label/c.Vacate";
+import BOOK_BUTTON_TITLE_LABEL from "@salesforce/label/c.Book_Room_Button_Title";
+import UNBOOK_BUTTON_TITLE_LABEL from "@salesforce/label/c.Unbook_Room_Button_Title";
+import ERROR_LABEL from "@salesforce/label/c.Error";
+
 export default class RoomsList extends LightningElement {
+  label = {
+    ROOMS_LIST_LABEL,
+    NUMBER_LABEL,
+    AREA_LABEL,
+    CAPACITY_LABEL,
+    RESERVED_LABEL,
+    CHANGE_BOOKING_LABEL,
+    BOOKED_LABEL,
+    FREE_LABEL,
+    ERROR_LABEL,
+    BOOK_BUTTON_TITLE_LABEL,
+    UNBOOK_BUTTON_TITLE_LABEL
+  }
+
   @api recordId;
 
   isManager;
   error;
 
-  bookButtonLabel = "Book";
-  vacateButtonLabel = "Vacate";
+  bookButtonLabel = BOOK_LABEL;
+  vacateButtonLabel = VACATE_LABEL;
 
   @wire(getObjectInfo, { objectApiName: OFFICE_ROOM_OBJECT })
   getOfficeRoomObject({ error, data }) {
     if (data) {
-      if (data.fields.Is_reserved__c.updateable) {
+      if (data.fields.Is_reserved__c && data.fields.Is_reserved__c.updateable) {
         this.isManager = true;
       } else {
         this.isManager = false;
